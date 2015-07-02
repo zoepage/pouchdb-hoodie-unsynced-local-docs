4090c86d7aedc6313411054aa7888fdc8eecd9ee
Gregor Martynus <gregor@martynus.net>
2015-07-02 11:11:44 -0400

    feat: initial version

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict'
+
+var exports = module.exports = { hoodieLocalChanges: hoodieLocalChanges }
+var EventEmitter = require('events').EventEmitter
+
+function hoodieLocalChanges (options) {
+  var state = {
+    emitter: new EventEmitter()
+  }
+
+  return {
+    db: this,
+    getLocalChanges: require('./lib/get-local-changes').bind(this, state, options)
+  }
+}
+
+/* istanbul ignore next */
+if (typeof window !== 'undefined' && window.PouchDB) {
+  window.PouchDB.plugin(exports)
+}
